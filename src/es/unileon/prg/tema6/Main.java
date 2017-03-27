283d81f122e9627123d70cf8623a5d2a508d9f07
Dondig00 <Dondig00@estudiantes.unileon.es>
2017-03-27 12:46:27 +0200

    Empezando

---
+++ src/es/unileon/prg/tema6/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+public static void main (String args []){
+	Date fecha = new Date(20,12,1967);
+	Date fecha2 = new Date(20,12,1950);
+	System.out.println(fecha.isSameMonth(fecha2));
+	
+
+}
+
+}
